615c1b97660de625508ba69510778ab4695e48e0
Caio Vaz <cvaz@acsicorp.com>
2022-02-22 22:56:47 -0700

    update

--- game/knightGame.py
+++ game/knightGame.py
@@ -12,37 +12,48 @@ class Goblin:
     name = "Goblin"
     healthPoints = 6
     attackStab = 3
+    attacks = [attackStab]
 
 class Mercenary:
     name = "Mercenary"
     healthPoints = choice ((5,12))
     attackWhip = choice ((1,2))
     attackSwing = choice ((3,5))
+    attacks = [attackWhip,attackSwing]
 
 class Troll:
     name = "Troll"
     healthPoints = choice ((10, 15))
     attackSwingClub = choice ((7, 10))
+    attacks = [attackSwingClub]
     
 class Cat:
     name = "Cat"
     healthPoints = choice ((6, 10))
     attackScratch = choice ((4, 7))
+    attacks = [attackScratch]
+
 
 class Turtle:
     name = "Turtle"
     healthPoints = choice ((5, 10))
     attackTurtleShell = choice ((5, 7))
+    attacks = [attackTurtleShell]
+
 
 class Whisard:
     name = "Wisard"
     healthPoints = choice ((5, 9))
     attackDarkPower = choice ((5, 9))
+    attacks = [attackDarkPower]
+
 
 class Weirdo:
     name = "Weirdo"
     healthPoints = choice ((6, 12))
     attackBananas = choice ((4, 10))
+    attacks = [attackBananas]
+
 
 playing = True
 
@@ -64,6 +75,22 @@ round = 1
 def picVillain():
     vSize = len(villains) - 1
     return villains[choice((0,vSize))]    
+
+def battle(hero,villian):
+    print("Hero > Villain")
+    print("\t Hero attacks power =", hero.attackFuryWhip)
+    print("\t Villian =", villian.name)
+    print("\t Villian current HP =", villian.healthPoints) 
+    dammage =  villian.healthPoints- hero.attackFuryWhip  
+    villian.healthPoints = dammage
+    print("\t Villian new HP =", villian.healthPoints)
+    print("Villain > Hero")
+    villainAttack = villain.attacks[0]
+    print("\t Villain attacks power =", villainAttack)
+    print("\t Hero current HP =", hero.healthPoints)
+    dammageHero = hero.healthPoints - villainAttack
+    hero.healthPoints = dammageHero
+    print("\t Hero new HP =", hero.healthPoints)
 
 while playing:
 
@@ -100,11 +127,12 @@ while playing:
     attack = input("Choose the Hero's attack: 0, 1, 2 \n")
     dammage = 0
     if attack == "0":
-        villian = picVillain()
-        dammage =  villian.healthPoints- knight.attackFuryWhip  
-        print("Villian =", villian.name)
-        print("Hero attacks power =", knight.attackFuryWhip)  
-        villian.healthPoints = dammage 
+        qtdVillainPicked = choice((2,5))
+        print("Qtd Villains= ",qtdVillainPicked)
+        for x in range(qtdVillainPicked):
+            villain = picVillain()
+            battle(knight,villain)
+           
              
     elif attack == "1":
         villian = picVillain()
@@ -133,11 +161,11 @@ while playing:
 
     # Here the villan attacks the Knigth
     #dammage = knight.healthPoints - goblin.attackStab
-    dammage = knight.healthPoints - picVillain() 
+    #dammage = knight.healthPoints - picVillain() 
     #dammage = knight.healthPoints - (goblin.attackStab + mercenary.attackSwing + troll.attackSwingClub + cat.attackScratch + turtle.attackTurtleShell + whisard.attackDarkPower + weirdo.attackBananas)
     #print("villains attacks power=", goblin.attackStab + mercenary.attackSwing + troll.attackSwingClub + cat.attackScratch + turtle.attackTurtleShell + whisard.attackDarkPower + weirdo.attackBananas)
     #print("villains attacks power=", 
-    knight.healthPoints = dammage
+    #knight.healthPoints = dammage
 
     
     round = round +1
